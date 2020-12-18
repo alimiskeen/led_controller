ab752db06e4385e47d8b5e6bee6185e5bb5b56f0
Raspberry Pi <alimiskeen@me.com>
2020-12-17 21:27:43 -0700

    added neopixel in the test file

--- test.py
+++ test.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
-# import neopixel
+import neopixel
 import board
 
-# pixels = neopixel.NeoPixel(board.)
+pixels = neopixel.NeoPixel(board.D18, 30)
+
+pixels[0] = (255, 0, 0)
 
 print(dir(board))
 
