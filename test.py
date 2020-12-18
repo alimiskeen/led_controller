f9cc07764c10f3ee7ff39d1d884fdc9d6def0af9
Ali A. Almiskeen <almiskee@eng.utah.edu>
2020-12-17 23:51:54 -0700

    adjusted test

--- test.py
+++ test.py
@@ -3,9 +3,13 @@
 import neopixel
 import board
 
-pixels = neopixel.NeoPixel(board.D18, 30)
+pixels = neopixel.NeoPixel(board.D18, 30, auto_write=False)
 
 pixels[0] = (255, 0, 0)
+
+pixels.brightness = .9
+
+pixels.show()
 
 print(dir(board))
 
