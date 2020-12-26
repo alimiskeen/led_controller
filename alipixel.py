2bbaf68f0bae4bbfb8492512d83006e6c3748919
Ali A. Almiskeen <almiskee@eng.utah.edu>
2020-12-25 22:48:30 -0700

    changed the fundamental way the alipixel classes work

--- alipixel.py
+++ alipixel.py
@@ -4,6 +4,16 @@ import neopixel
 import threading
 import time
 
+# global values
+ORDER = neopixel.GRB
+pixel_pin = board.D18
+num_pixels = (150*18) + 18
+
+
+def delegate_format(self: neopixel.NeoPixel, input_list: list) -> None:
+    item = input_list[0]
+    self.fill((255, 0, 0))
+
 
 def xy_to_index(x: int, y: int) -> int:
     """
@@ -20,36 +30,36 @@ def xy_to_index(x: int, y: int) -> int:
     return index
 
 
-def basic_color_wheel(x: int, y: int, inputs: list) -> tuple:
-    if inputs[0] < 10000:
-        inputs[0] += 1
-        return 255, 0, 0
-    elif inputs[0] < 20000:
-        inputs[0] += 1
-        return 0, 255, 0
-    elif inputs[0] < 30000:
-        inputs[0] += 1
-        return 0, 0, 255
-    else:
-        inputs[0] = 0
-        return 255, 0, 0
+# def basic_color_wheel(x: int, y: int, inputs: list) -> tuple:
+#     if inputs[0] < 10000:
+#         inputs[0] += 1
+#         return 255, 0, 0
+#     elif inputs[0] < 20000:
+#         inputs[0] += 1
+#         return 0, 255, 0
+#     elif inputs[0] < 30000:
+#         inputs[0] += 1
+#         return 0, 0, 255
+#     else:
+#         inputs[0] = 0
+#         return 255, 0, 0
 
 
-class AliPixel:
+class Screen:
 
-    def __init__(self, drawing_method=basic_color_wheel, drawing_variable=[0]):
-        self.pixels = neopixel.NeoPixel(board.D18, (150 * 18 + 10), auto_write=False)
-        self.drawing_method = drawing_method
+    def __init__(self, num_of_pixels, drawing_method=delegate_format, drawing_variable=None):
+        if drawing_variable is None:
+            drawing_variable = [0]
+        self.pixels = neopixel.NeoPixel(board.D18, num_of_pixels, auto_write=False)
+        neopixel.NeoPixel.drawing_method = drawing_method
+        # self.pixels.drawing_method = drawing_method
         self.drawing_variable = drawing_variable
 
     def _draw(self):
-        if self.drawing_method is None:
-            raise Exception(" can't draw without an external drawing method")
-
         while True:  # TODO: implement timing for frame drawing
-            for y in range(19):
-                for x in range(150):
-                    self.pixels[xy_to_index(x, y)] = self.drawing_method(x, y, self.drawing_variable)
+
+            self.pixels.drawing_method(self.drawing_variable)
+
             self.pixels.show()  # TODO: try to maybe use a delegate per frame instead of a delegate per led
             time.sleep(33 / 1000)  # TODO: fix the timing, from rigid to remaining time to next frame
 
@@ -60,5 +70,5 @@ class AliPixel:
 
 if __name__ == '__main__':
     print("starting threaded pixel display")
-    screen = AliPixel()
+    screen = Screen((150 * 19) + 10)
     screen.main_loop()
