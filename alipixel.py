a85fb72220c07358407ad71ad5957518c29818fe
Ali A. Almiskeen <almiskee@eng.utah.edu>
2020-12-25 23:30:46 -0700

    wrote a new delegate to lightup the top of the desk

--- alipixel.py
+++ alipixel.py
@@ -7,12 +7,25 @@ import time
 # global values
 ORDER = neopixel.GRB
 pixel_pin = board.D18
-num_pixels = (150*18) + 10
+num_pixels = (150 * 18)
 
 
 def delegate_format(self: neopixel.NeoPixel, input_list: list) -> None:
     item = input_list[0]
-    self.fill((100, 10, 50))
+    self.fill((100, 100, 100))
+
+
+def eclipse_equation(x: int, y: int) -> bool:
+    left = ((x - 75) ** 2) / (50 ** 2)
+    right = ((x - 18) ** 2) / (8 ** 2)
+    return (left + right) < 1
+
+
+def lightup_desk(self: neopixel.NeoPixel, input_list: list) -> None:
+    for y in range(18):
+        for x in range(150):
+            if eclipse_equation(x, y):
+                self[xy_to_index(x, y)] = (100, 100, 100)
 
 
 def xy_to_index(x: int, y: int) -> int:
@@ -30,46 +43,31 @@ def xy_to_index(x: int, y: int) -> int:
     return index
 
 
-# def basic_color_wheel(x: int, y: int, inputs: list) -> tuple:
-#     if inputs[0] < 10000:
-#         inputs[0] += 1
-#         return 255, 0, 0
-#     elif inputs[0] < 20000:
-#         inputs[0] += 1
-#         return 0, 255, 0
-#     elif inputs[0] < 30000:
-#         inputs[0] += 1
-#         return 0, 0, 255
-#     else:
-#         inputs[0] = 0
-#         return 255, 0, 0
-
-
 class Screen:
 
-    def __init__(self, num_of_pixels, drawing_method=delegate_format, drawing_variable=None):
+    def __init__(self, num_of_pixels, pin, drawing_method=delegate_format, drawing_variable=None):
         if drawing_variable is None:
             drawing_variable = [0]
-        self.pixels = neopixel.NeoPixel(board.D18, num_of_pixels, auto_write=False)
+        self.pixels = neopixel.NeoPixel(pin, num_of_pixels, auto_write=False)
         neopixel.NeoPixel.drawing_method = drawing_method
-        # self.pixels.drawing_method = drawing_method
         self.drawing_variable = drawing_variable
 
     def _draw(self):
         while True:  # TODO: implement timing for frame drawing
-
             self.pixels.drawing_method(self.drawing_variable)
-
             self.pixels.show()  # TODO: try to maybe use a delegate per frame instead of a delegate per led
-            time.sleep(33 / 1000)  # TODO: fix the timing, from rigid to remaining time to next frame
+            # time.sleep(33 / 1000)  # TODO: fix the timing, from rigid to remaining time to next frame
             print('drawn')
 
     def main_loop(self):
         thr = threading.Thread(target=self._draw)
         thr.start()
 
+    def change_method(self, method):
+        neopixel.NeoPixel.drawing_method = method
+
 
 if __name__ == '__main__':
     print("starting threaded pixel display")
-    screen = Screen(num_pixels)
+    screen = Screen(num_pixels, pixel_pin, drawing_method=lightup_desk)
     screen.main_loop()
